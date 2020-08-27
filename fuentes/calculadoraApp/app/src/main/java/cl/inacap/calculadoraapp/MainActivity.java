d048acbafd587bbc201759a7bf7c544fafed2af0
diegof <diegolimon123@gmail.com>
2020-08-27 12:03:54 -0400

    Generar un hola mundo de layout
    generar un linear layout en main activity
    agregar textView con peso
    agregar boton
    generar toast al presionar boton

---
+++ fuentes/calculadoraApp/app/src/main/java/cl/inacap/calculadoraapp/MainActivity.java
@@ -0,0 +1,19 @@
+package cl.inacap.calculadoraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void saludar(View v){
+        Toast.makeText(this, "hola Al diego ", Toast.LENGTH_SHORT).show();
+    }
+}
